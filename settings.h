60c8cbd5e6eda0525a23299956b9c39360143457
cing01 <142486835+cing01@users.noreply.github.com>
2023-08-24 13:35:00 +0700

    Update settings.h
    Hongcingjung01

--- settings.h
+++ settings.h
@@ -1,7 +1,7 @@
 // TODO: create an interface to change these
 
-const char ssid[] = "YOURSSID";  //  your network SSID (name)
-const char pass[] = "YOURPASS";       // your network password
+const char ssid[] = "Hongcingjung01";  //  your network SSID (name)
+const char pass[] = "Hong0101cingjung";       // your network password
 
 const char* ntpServerName = "ntp.example.com"; // NTP server hostname - see http://www.pool.ntp.org/en/vendors.html
 #define NTP_INTERVAL 1024  // seconds between polling the NTP server - check with your ntp operator before lowering this
